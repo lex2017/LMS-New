09fae9fd08e36e061f48583284ff828b30439ffe
K A <ka_andersson@outlook.com>
2017-03-30 16:00:16 +0200

    Change display names and fix ActivityType model

--- LexiconLMS/Models/ActivityType.cs
+++ LexiconLMS/Models/ActivityType.cs
@@ -12,6 +12,6 @@ namespace LexiconLMS.Models
         [Display(Name="Aktivitetstyp")]
         public string Name { get; set; }
         public int? ActivityId { get; set; }
-        public virtual Activity Activity { get; set; }
+        public virtual ICollection<Activity> Activity { get; set; }
     }
 }
