a038d2a430531b9f0f675a32dd5a09f1a0f835ed
K A <ka_andersson@outlook.com>
2017-03-30 15:24:35 +0200

    Modify student views

--- LexiconLMS/Models/ActivityType.cs
+++ LexiconLMS/Models/ActivityType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace LexiconLMS.Models
     public class ActivityType
     {
         public int ActivityTypeID { get; set; }
+        [Display(Name="Aktivitetstyp")]
         public string Name { get; set; }
         public int? ActivityId { get; set; }
         public virtual Activity Activity { get; set; }
